31ae8837b023d11db1391ce926e2e7d1af415bde
urbanaut <bwitt4@gmail.com>
2016-06-15 16:16:12 -0600

    Grammar edit

--- verify-page-spec.js
+++ verify-page-spec.js
@@ -1,8 +1,8 @@
 /*
- Start the WebDriver Server using the command: webdriver-manager start --standalone
+ Start the WebDriver Server using the command, "webdriver-manager start --standalone""
  from within the project folder.
- 
- Kick off test using the command: protractor conf.js
+
+ Kick off test using the command, "protractor conf.js""
 */
 
 
